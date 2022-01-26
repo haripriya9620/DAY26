4c828f0aa66253be6165b9557b132938676d7a86
Haripriya Sidda <swathi9620@gmail.com>
2022-01-26 21:28:44 +0530

    PricingCard Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PriceCard from "./Components/PriceCard";
 
 function App() {
+  let pricingData = [
+    { 
+      tagName: "FREE",
+      price: 0,
+      user: "Single User",
+      userFlag:true,
+      storage: "5GB Storage",
+      storageFlag:true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsFlag:true,
+      access: "Community Access",
+      accessFlag:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsFlag:false,
+      support:"Dedicated Phone Support",
+      supportFlag:false,
+      domain:"Free Subdomain",
+      domainFlag:false,
+      report:"Monthly Status Reports",
+      reportFlag:false
+    },
+    { 
+      tagName: "PLUS",
+      price: 9,
+      user: "5 Users",
+      userFlag:true,
+      storage: "50GB Storage",
+      storageFlag:true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsFlag:true,
+      access: "Community Access",
+      accessFlag:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsFlag:true,
+      support:"Dedicated Phone Support",
+      supportFlag:true,
+      domain:"Free Subdomain",
+      domainFlag:true,
+      report:"Monthly Status Reports",
+      reportFlag:false
+    },
+    { 
+      tagName: "PRO",
+      price: 49,
+      user: "Unlimited Users",
+      userFlag:true,
+      storage: "150GB Storage",
+      storageFlag:true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsFlag:true,
+      access: "Community Access",
+      accessFlag:true,
+      privateProjects:"Unlimited Private Projects",
+      privateProjectsFlag:true,
+      support:"Dedicated Phone Support",
+      supportFlag:true,
+      domain:"Unlimited Free Subdomains",
+      domainFlag:true,
+      report:"Monthly Status Reports",
+      reportFlag:true
+    }
+    
+  ] 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            {
+              pricingData.map((e)=>{
+                return <PriceCard data={e}/>
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
